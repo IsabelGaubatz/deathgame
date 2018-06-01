af80cf24e172aacff67ee5dd8364729a81eae6eb
Romina Marsico <rominamarsico@users.noreply.github.com>
2018-06-01 13:57:09 +0200

    Figur springt und duckt sich

--- hello_world.py
+++ hello_world.py
@@ -4,102 +4,96 @@
 # Player sprite and movement
 import pygame
 
-
-
 WIDTH, HEIGHT = 800, 480
-HW, HH = WIDTH/2, HEIGHT/2
+HW, HH = WIDTH / 2, HEIGHT / 2
 x = 0
 FPS = 60
-
-# define colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
 
 # initialize pygame and create window
 pygame.init()
 pygame.mixer.init()
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("HelloWorld")
+pygame.display.set_caption("DeathGame")
 bg = pygame.image.load("JungleAssetPack/parallaxBackground/plx-5.png").convert()
 bgWidth, bgHeight = bg.get_rect().size
 bg_size = pygame.transform.scale(bg, (800, 480))
 bgScaleWidth = bg_size.get_rect().width
 
+stageWidth = bgScaleWidth * 2  # wird nicht verwendet
 
-stageWidth = bgScaleWidth*2
-
-
-startScrollingPosX = HW
-
+startScrollingPosX = HW  # wird nicht verwendet
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
 
-        self.image = pygame.image.load("JungleAssetPack/Character/sprites/idle.gif").convert_alpha()#pygame.Surface((50, 40))
-        self.imageflip = pygame.transform.flip(self.image, True, False)
+        self.image = pygame.image.load(
+            "JungleAssetPack/Character/sprites/idle.gif").convert_alpha()  # pygame.Surface((50, 40))
+        self.imageRun = pygame.image.load("JungleAssetPack/Character/sprites/run.gif").convert_alpha()
+        self.imageflip = pygame.transform.flip(self.imageRun, True, False)
         self.playerwidth = self.image.get_width()
         self.playerheight = self.image.get_height()
         self.playerposx = self.playerwidth
-        #self.image.fill(GREEN)
+        self.newHeight = pygame.transform.scale(self.image, (19, 17))
+        # self.image.fill(GREEN)
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2
-        self.rect.bottom = HEIGHT - 10
+        self.rect.bottom = HEIGHT - 50
         self.speedx = 0
         self.speedy = 0
         self.stageposx = 0
         self.x = 0
 
-
-
-
     def update(self):
 
-            self.speedx = 0
-            self.speedy = 0
-            keystate = pygame.key.get_pressed()
-            if keystate[pygame.K_LEFT]:
-                self.speedx = -8
-                self.image = self.imageflip
-            elif keystate[pygame.K_RIGHT]:
-                self.speedx = 8
-                self.image = pygame.image.load("JungleAssetPack/Character/sprites/run.gif").convert_alpha()
-            elif keystate[pygame.K_DOWN]:
-                self.speedy = 8
+        self.speedx = 0
+        self.speedy = 0
+
+        # Key pressed: constant movement
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -8
+            self.image = self.imageflip
+        elif keystate[pygame.K_RIGHT]:
+            self.speedx = 8
+            self.image = pygame.image.load("JungleAssetPack/Character/sprites/run.gif").convert_alpha()
+        elif keystate[pygame.K_DOWN]:
+            # self.image = pygame.image.load("JungleAssetPack/Character/sprites/landing.png").convert_alpha()
+            self.image = self.newHeight
+            self.rect.bottom = HEIGHT - 50 + 19
+        else:
+            self.image = pygame.image.load("JungleAssetPack/Character/sprites/idle.gif").convert_alpha()
+            self.rect.bottom = HEIGHT - 50
+
+        # Key pressed: only one move
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                self.rect.bottom = HEIGHT - 100
                 self.image = pygame.image.load("JungleAssetPack/Character/sprites/jump.png").convert_alpha()
-            elif keystate[pygame.K_UP]:
-                self.speedy = -8
-                self.image = pygame.image.load("JungleAssetPack/Character/sprites/landing.png").convert_alpha()
-            else:
-                self.image = pygame.image.load("JungleAssetPack/Character/sprites/idle.gif").convert_alpha()
 
+        # Key release: only one move
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                self.rect.bottom = HEIGHT - 50
 
-            self.rect.x += self.speedx
-            if self.rect.right > WIDTH: #kann man verwenden um zu schauen, wann das Hintergrundbild weiter läuft
-                self.rect.right = WIDTH
-            if self.rect.left < 0:
-                self.rect.left = 0
+        self.rect.x += self.speedx
+        if self.rect.right > WIDTH:  # kann man verwenden um zu schauen, wann das Hintergrundbild weiter läuft
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
 
-            self.rect.y += self.speedy
-            if self.rect.bottom > HEIGHT:
-                self.rect.bottom = HEIGHT
-            if self.rect.top < 0:
-                self.rect.top = 0
-
-
-
+        self.rect.y += self.speedy
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+        if self.rect.top < 0:
+            self.rect.top = 0
 
 
 all_sprites = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
-
 
 # Game loop
 running = True
@@ -111,7 +105,6 @@ while running:
         screen.blit(bg_size, (rel_x, 0))
     x -= 1
 
-
     # keep loop running at the right speed
     clock.tick(FPS)
     # Process input (events)
@@ -119,7 +112,7 @@ while running:
         # check for closing window
         if event.type == pygame.QUIT:
             running = False
-    #screen.blit(bg_size, [0, 0])
+    # screen.blit(bg_size, [0, 0])
 
     # Update
     all_sprites.update()
@@ -131,4 +124,3 @@ while running:
     pygame.display.flip()
 
 pygame.quit()
-
